66cf85fe9ac26e6eeb8fc35d5287b814eb0e57ae
fungusflip <filip.renemark@student.forsbergsskola.se>
2024-03-05 14:15:52 +0100

    LightCalculations

--- OpenGLGame/ColorLightningFragmentShader.glsl
+++ OpenGLGame/ColorLightningFragmentShader.glsl
@@ -9,10 +9,19 @@ in vec3 FragPos;
 
 void main()
 {
+    vec3 lightColor = vec3( 1,1,1);
+
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);  
     float diff = max(dot(norm, lightDir), 0.0);
-    
+    vec3 diffuse = diff * lightColor;
+    vec3 result = (ambient + diffuse) * pixelColor.rgb;
+    pixelColor = vec4(result, 1.0);
+
+
     pixelColor = vertexColor;
 
 }
